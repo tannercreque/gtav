2334999a586668c29eabf96b956ffe8018a6f2bb
Tanner <tanner.creque@gmail.com>
2021-09-02 10:15:43 -0600

    working commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Vehicle.destroy_all
+print "Destroying all vehicles..."
+
+Vehicle.create(
+    name: "2012 Nissan GTR",
+    speed: "125",
+    category: "Super",
+    code: "2012gtr",
+    price: 1100000 ,
+)
+Vehicle.create(
+    name: "Adder",
+    speed: "130",
+    category: "Super",
+    code: "adder",
+    price: 1900000 ,
+)
+Vehicle.create(
+    name: "Akuma",
+    speed: "95",
+    category: "Motorcycles",
+    code: "akuma",
+    price: 9000 ,
+)
